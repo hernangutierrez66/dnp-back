0ca8376f2da51ad888280048d10794228ba54c82
Hernán Gutierrez <hernan.gutierrez@jikkosoft.com>
2023-05-17 18:07:31 -0500

    :hammer: eliminiacion de codigo comentado

--- src/main/java/repositories/HierarchyTypeRepository.java
+++ src/main/java/repositories/HierarchyTypeRepository.java
@@ -10,7 +10,7 @@ import java.util.List;
 @Repository
 public interface HierarchyTypeRepository extends JpaRepository<HierarchyType, Integer> {
 
-    //List<Hierarchy> findByType(HierarchyType hierarchyType);
+    
 
     //List<Hierarchy> findByParent(HierarchyType hierarchyType);
 }
